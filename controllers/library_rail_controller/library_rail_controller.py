2b3dc9f992357bc15c19e147cf2419342c910128
SamD3087 <srd118@student.bham.ac.uk>
2025-12-10 11:22:37 +0000

    addedOccupancyFunctions

--- controllers/library_rail_controller/library_rail_controller.py
+++ controllers/library_rail_controller/library_rail_controller.py
@@ -36,6 +36,18 @@ for name in arm_names:
     motor = robot.getDevice(name)
     motor.setVelocity(1.0) # so the arm doesn't go too fast
     arm_motors.append(motor)
+    
+        
+# 3. Setup Lidar Scanners
+lid1 = robot.getDevice("lidar1")
+lid2 = robot.getDevice("lidar2")
+lid1.enable(timestep)
+lid2.enable(timestep)
+lid1Res = lid1.getHorizontalResolution()
+lid2Res = lid2.getHorizontalResolution()
+#Setup gps (used to test Occupancy Map seperate from Odometry)
+gps = robot.getDevice("gps")
+gps.enable(timestep)
 
 # initialise grippers
 finger1 = robot.getDevice('finger::left')
@@ -79,13 +91,115 @@ def set_gripper(width):
         finger1.setPosition(width)
         finger2.setPosition(width) 
         
+# functions to convert coordinates to array indexes       
+def get_x_index(x, enviromentLength):
+    approxindex = ((x/0.01) + (enviromentLength/2))
+    index = math.floor(approxindex)
+    return index
+    
+def get_y_index(y, enviromentHeight):
+    approxindex = -((y/0.01) - (enviromentHeight/2))
+    index = math.floor(approxindex)
+    return index
+
+# function to convert array indexes to approximate coordinates
+def get_x_coordinate(x_index, environmentLength):
+    approxheight = ((0.01*x_index) - (environmentHeight/0.02))
+    x = approxheight+0.005
+    return x
+    
+def get_y_coordinate(y_index, environmentHeight):
+    approxheight = ((environmentHeight/0.02) - (0.01*y_index))
+    y = approxheight-0.005
+    return y
+
+# function to create array of angles representing each lidar scan
+def get_angle_array(robotFN, lidarFRN, lid):
+    res = lid.getHorizontalResolution()
+    fov = lid.getFov()
+    array = []
+    for i in range(res):
+        # sets initial value to angle of first lidar scan
+        if i == 0:
+            array.append(robotFN + lidarFRN)
+        else:
+            # increments angle based on lidar field of view and resolution
+            angle = array[i-1] + (fov / (res - 1))
+            # 
+            if (angle > 2*math.pi):
+                array.append(angle - (2*math.pi))
+            else:
+                array.append(angle)
+            
+    return array
+    
+# creates an occupancy grid containing just zeros    
+def create_empty_occupancy_grid(enHeight, enLength):
+    array = [[0 for _ in range(enLength)] for _ in range(enHeight)]
+    return array 
+
+
+def update_occupancy_grid(robotx, roboty, robotFN, lidarFRN, lid, occupancyGrid):
+
+    #print("updating Occupancy Grid....")
+    res = lid.getHorizontalResolution()
+    #print(res)
+    scan = lid.getRangeImage()
+    #print(scan)
+    angle = get_angle_array(robotFN, lidarFRN, lid)
+    #print(angle)
+    
+    for i in range(res):
+        if (math.isinf(scan[i])):
+            pass
+            
+        else:
+            x = 0
+            y = 0
+            
+            if (0 <= angle[i] < (math.pi/2)):
+                x = (scan[i] * math.sin(angle[i]))
+                y = (scan[i] * math.cos(angle[i]))
+            elif ((math.pi/2) <= angle[i] < (math.pi)):
+                x = (scan[i] * math.cos(angle[i] - (math.pi/2)))
+                y = -(scan[i] * math.sin(angle[i] - (math.pi/2)))
+            elif (math.pi <= angle[i] < (3*math.pi/2)):
+                x = -(scan[i] * math.sin(angle[i] - math.pi))
+                y = -(scan[i] * math.cos(angle[i] - math.pi))
+            else:
+                x = -(scan[i] * math.sin((2*math.pi) - angle[i]))
+                y = (scan[i] * math.cos((2*math.pi) - angle[i]))
+                                
+            x = x + robotx
+            y = y + roboty
+                        
+            index_x = get_x_index(x, len(occupancyGrid[0]))
+            index_y = get_y_index(y, len(occupancyGrid))
+                        
+            occupancyGrid[index_y][index_x] = 1
+            
+    return occupancyGrid
+    
+
+        
 counter = 0
 
 set_arm_pose(0.0, 1.1, -2.5, 1.5, 0.0) 
 set_gripper(0.024) 
 
+#Creates an Occupancy Grid for each shelf level
+grid1 = create_empty_occupancy_grid(environmentHeight, environmentLength)
+grid2 = create_empty_occupancy_grid(environmentHeight, environmentLength)
+
 while robot.step(timestep) != -1:
     counter += 1
+    
+    # Sets robotCoordinates from gps (used to test Occupancy map seperate from odometry)
+    robotCoordinates = gps.getValues()
+ 
+    # Updates each Occupancy Grid
+    grid1 = update_occupancy_grid(robotCoordinates[0], robotCoordinates[1], RobotsAngleFromNorth, LidarAngleFromRobot, lid1, grid1)
+    grid2 = update_occupancy_grid(robotCoordinates[0], robotCoordinates[1], RobotsAngleFromNorth, LidarAngleFromRobot, lid2, grid2)
     
     if counter < 100:
         # Move Forward
@@ -170,5 +284,10 @@ while robot.step(timestep) != -1:
     elif counter == 2450: 
         set_arm_pose(0.0,0.8, -1.0,-1.5,0.0) 
         print("Done :)")
-    
+        
+        #Prints Occupancy Map (for testing)
+        for y in range(environmentHeight):
+            for x in range(environmentLength):
+                print(grid1[y][x], end="")
+            print()
     
